f417d74b8b78619b8a668eadc8724d6bd2665ffa
digitalchat1 <167578042+digitalchat1@users.noreply.github.com>
2025-04-25 07:43:36 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,40 @@
+let data = {
+  sections: [
+    {
+      id: "mobile",
+      name: "Mobile Packages",
+      packages: [
+        { id: "p1", name: "Super Kix 25", priceBeforeTax: 21.93, priceAfterTax: "25.00" },
+        { id: "p2", name: "Super Kix 35", priceBeforeTax: 30.70, priceAfterTax: "35.00" },
+        { id: "p3", name: "Super Kix 60", priceBeforeTax: 52.63, priceAfterTax: "60.00" }
+      ]
+    },
+    {
+      id: "internet",
+      name: "Home Internet",
+      packages: [
+        { id: "p4", name: "WE Space 140", priceBeforeTax: 209.65, priceAfterTax: "239.40" },
+        { id: "p5", name: "WE Space 200", priceBeforeTax: 289.47, priceAfterTax: "330.60" },
+        { id: "p6", name: "WE Space 250", priceBeforeTax: 359.65, priceAfterTax: "410.40" }
+      ]
+    },
+    {
+      id: "landline",
+      name: "Landline Services",
+      packages: [
+        { id: "p7", name: "WE Talk 15", priceBeforeTax: 13.16, priceAfterTax: "15.00" },
+        { id: "p8", name: "WE Talk 25", priceBeforeTax: 21.93, priceAfterTax: "25.00" },
+        { id: "p9", name: "WE Talk 40", priceBeforeTax: 35.09, priceAfterTax: "40.00" },
+        { id: "p10", name: "Installation Fee", priceBeforeTax: 178.42, priceAfterTax: "203.40" }
+      ]
+    },
+    {
+      id: "addons",
+      name: "Add-On Services",
+      packages: [
+        { id: "p11", name: "Extra Quota 10", priceBeforeTax: 8.77, priceAfterTax: "10.00" },
+        { id: "p12", name: "Extra Quota 20", priceBeforeTax: 17.54, priceAfterTax: "20.00" }
+      ]
+    }
+  ]
+};
